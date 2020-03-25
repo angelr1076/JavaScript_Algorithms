8edc0204dee5fa418e9c8df36e46aeefe7883225
Angel Rodriguez <angelr1076@gmail.com>
2020-03-25 08:14:57 -0400

    adding all other markdown files

---
+++ factorialize/README.md
@@ -0,0 +1,11 @@
+# FreeCodeCamps Algorithm Exercises
+
+Return the factorial of the provided integer.
+
+If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
+
+Factorials are often represented with the shorthand notation n!
+
+For example: 5! = 1 x 2 x 3 x 4 x 5 = 120
+
+Only integers greater than or equal to zero will be supplied to the function.
--- factorialize/factorialize.js
+++ factorialize/factorialize.js
@@ -1,14 +1,3 @@
-// From FreeCodeCamps Algorithm Exercises
-// Return the factorial of the provided integer.
-
-// If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
-
-// Factorials are often represented with the shorthand notation n!
-
-// For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
-
-// Only integers greater than or equal to zero will be supplied to the function.
-
 function factorialize(num) {
     // Set the base of recursion and return only 1 if the number is 0 or 1
     if (num == 0 || num == 1) {
